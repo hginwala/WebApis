2d773fb2a77f2db587e4fa739d9d9392b91dad83
--psymagic <hardik.ginwala@gmail.com>
2013-03-21 00:07:11 -0400

    intital check in for paisleymagic

---
+++ WebApis/Models/Products/ProductListingModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApis.Models.Products
+{
+    public class ProductListingModel
+    {
+        public List<ProductsModel> Products = new List<ProductsModel>();
+        public string CategoryName { get; set; }
+    }
+}
